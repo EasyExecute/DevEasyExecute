ce543524ca6a43f061fe43d9b4b0603b35b89d4b
contactsamie <contactsamie@gmail.com>
2017-03-18 17:36:06 -0400

    update

--- Src/build.fsx
+++ Src/build.fsx
@@ -64,7 +64,7 @@ let NugetDeployPath= match nugetDeployPath with
                            | _         -> "-"+buildParam
 
 // version info
-let version = "0.1.80" 
+let version = "0.0.2-pre" 
 
 // Targets
 Target "Clean" (fun _ -> 
